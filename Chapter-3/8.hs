0be9635e1e1798127576612bd20ba8caf90bfb64
Vani <echooatmeal@gmail.com>
2015-01-15 12:22:03 +0800

    add function signature to getHeight

--- Chapter-3/8.hs
+++ Chapter-3/8.hs
@@ -2,5 +2,6 @@ data Tree a = Node a (Tree a) (Tree a)
             | Empty
               deriving (Show)
 
+getHeight :: Tree a -> Int
 getHeight Empty = 0
 getHeight (Node now l r) = max (getHeight l) (getHeight r) + 1
